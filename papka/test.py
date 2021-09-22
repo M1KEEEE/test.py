b4b3584df1f5409fa22164de9e8cdffcd63e19c4
M1KEEEE <72807912+M1KEEEE@users.noreply.github.com>
2021-09-22 16:29:17 +0300

    Add files via upload

--- papka/test.py
+++ papka/test.py
@@ -1,1 +1,4 @@
-
+otvet_polsovatelya = input("Куда ехать? ")
+if otvet_polsovatelya == "1" or otvet_polsovatelya == "2" or otvet_polsovatelya == "3":
+    print("выбрал дорого 1, 2, 3")
+print("Конец")

16f12a5dceba256adf5216cd1750f06c6c9e583f
M1KEEEE <72807912+M1KEEEE@users.noreply.github.com>
2021-09-25 12:26:46 +0300

    Add files via upload

--- papka/test.py
+++ papka/test.py
@@ -1,10 +1,14 @@
 level = "0"
 user_choise = ""
+way1 = "0"
+way2 = "0"
+way3 = "0"
 
-print("У камня")
-print("1- ")
-print("2- ")
-print("3- ")
+print("Вы находитесь у камня")
+print("Выбирайте свой путь")
+print("1 - ")
+print("2 - ")
+print("3 - ")
 
 while user_choise not in("1", "2", "3"): 
     user_choise = input("Что вы выберете?")
@@ -13,14 +17,27 @@ level = level +"1"
 
 if level == "01":
     print("дорога 1")
-    print("На вас напали")
+    print("Сделайте свой выбор")
     print("Варианты")
+    print("1 - ")
+    print("2 - ")
+    print("3 - ")
 
 elif level == "02":
     print("дорога 2")
+    print("Сделайте свой выбор")
+    print("Варианты")
+    print("1 - ")
+    print("2 - ")
+    print("3 - ")
 
 else: 
     print("дорога 3")
+    print("Сделайте свой выбор")
+    print("Варианты")
+    print("1 - ")
+    print("2 - ")
+    print("3 - ")
 
 user_choise = ""
 while user_choise not in("1", "2"): 

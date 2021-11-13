8305c3836aa930153e14c036037ebd9b5f031bae
M1KEEEE <72807912+M1KEEEE@users.noreply.github.com>
2021-11-13 13:42:28 +0300

    Add files via upload

--- papka/main.py
+++ papka/main.py
@@ -1,63 +1,32 @@
-import os
-import random
-import dice
+import shop
+from os import system
 
-user_name = "Вася"
-user_money = 5000 
+user_name = "Вася Питонов"
+user_money = 5000
+user_xp = 0
+user_level = 1
+user_inventory = []
+user_inventory.append("меч")
+user_inventory.append("конь")
+user_inventory.append("щит")
+game = true
 
-def create_user():
-    user_name = "Вася"
-    user_money = 5000 
-    print("при создании персонажа у меня было", user_money)
-
-
-def show_location_home():
-    """
-        Попадаем на начальную локацию - дом
-    """
+def show_location_home(user_name, user_money, user_xp, user_level, user_inventory):
+    while game:
     os.system("cls")
-    print("Вы дома")
-    print("1 - в казино")
-    print("2 - ждать")
+        print(f"имя:{user_money}")
+        print(f"деньги:{user_money}")
+        print("инвентарь:")
+        for item in user_inventory:
+            print("•", item)
+        print("-------------------")
+        print(f"{user_name} сидит на базе!")
+        print(f"1 - пойти в магазин ")
+        print(f"2 - уйти")
 
-    # спрашиваем у пользователя
-    user_choice = ""
-    while user_choice not in ("1", "2"):
-        user_choice = input("введите номер варианта и нажмите Enter ")
-
-    # проверяем ответ пользователя
-    if user_choice == "1":
-        show_location_casino()
-    else:
-        print("жду")
-        os.system("cls")
-        show_location_home()
-
-def show_location_casino():
-    """
-        Попадаем на локацию казино
-    """
-    os.system("cls")
-    print("Вы в казино")
-    print("1 - домой")
-    print("2 - ждать")
-    print("3 - сыграть")
-
-    # спрашиваем у пользователя
-    user_choice = ""
-    while user_choice not in ("1", "2", "3"):
-        user_choice = input("введите номер варианта и нажмите Enter ")
-
-    # проверяем ответ пользователя
-    if user_choice == "1":
-        show_location_home()
-    if user_choice == "2":
-        print("жду")
-        os.system("cls")
-        show_location_casino()
-    if user_choice == "3":
-        dice.play_dice(user_money)
+    user_choise = ""
+    while user_choise not in ("1", "2"):
+        user_choise = input("Что будем делать?")
 
 # игра началась здесь
-create_user()
 show_location_home()
--- papka/shop.py
+++ papka/shop.py
@@ -1,32 +1,16 @@
-import os
+from os import system
+import main
 
-
-user_name = "Вася Питонов"
-user_money = 1000
-user_inventory = []
-
-def show_location_shop(user_name: str, user_money: int, user_inventory: int, user_choise):
-	is_shopping == True
-	potion_price = 100
-	while is_shopping:
-		os.system("cls")
-		print(f"{user_name} зашел в лавку алхимика Варенцова. У него можно купить зелья со странным запахом.")
-		print(f"1 - купить зелье за {potion_price}")
-		print(f"2 - уйти")
-
-		user_choise = "0"
-		while user_choise not in ["1", "2"]:
-			user_choise = input("Что будем делать?")
-
-		if user_choise == "1" and user_money >= potion_price:
-			user_money -= potion_price
-			user_inventory.append(potion)
-			print(f"{user_name} купил зелье")
-		elif user_choise == "1":
-		elif
-		else:
-			is_shopping = False
-
-
-
-show_location_shop(user_name, user_money, user_inventory)
+def show_location_shop(user_name, user_money, user_inventory):
+    is_busy = True
+    while is_busy:
+        system("cls")
+        potion_price = 500
+    if user_choise == "1" and user_money >= potion_price:
+        user_money -= potion_price
+        user_inventory.append("зелье")
+    elif user_choise == "1" and user_money < potion_price:
+        is_busy = False
+        print("Не хватает денег!")
+        input("ENTER - чтобы продолжить")
+        return user_money
